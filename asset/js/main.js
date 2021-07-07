8734284093315180f4317d0cd49235ac0d5368d4
fayeakuzzaman <fayeakuzzamantonmoy@gmail.com>
2021-07-07 23:49:03 +0600

    work with header section

---
+++ asset/js/main.js
@@ -0,0 +1,8 @@
+(function ($){
+    "use  strict";
+    $("[data-background]").each(function (){
+        $(this).css("background-image","url(" + $(this).attr("data-background") + ")")
+    })
+    
+
+})(jQuery);
