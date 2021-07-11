5d6dd877d0d2cf66d44ffe2fa5900ed14444fe16
fayeakuzzaman <fayeakuzzamantonmoy@gmail.com>
2021-07-11 23:12:43 +0600

    complete header section

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,5 +1,9 @@
 (function ($){
     "use  strict";
+
+
+    // data - background
+    
     $("[data-background]").each(function (){
         $(this).css("background-image","url(" + $(this).attr("data-background") + ")")
     })
